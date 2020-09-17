9579855c1f2cac0da6ca0c97370e3873d632026a
Jake Flynn <jakepflynn@gmail.com>
2020-09-17 12:52:22 +0100

    fix impact

--- BigDebuffs.lua
+++ BigDebuffs.lua
@@ -124,7 +124,7 @@ BigDebuffs.Spells = {
 		[61721] = { type = "cc", },
 	[42950] = { type = "cc", },  -- Dragon's Breath
 	[44572] = { type = "cc", }, -- Deep Freeze
-	[64343] = { type = "cc", }, -- Impact
+	[12355] = { type = "cc", }, -- Impact
 	[45438] = { type = "immunities", },  -- Ice Block
 	[12494] = { type = "roots", },  -- Frostbite
 	[42917] = { type = "roots", },  -- Frost Nova
